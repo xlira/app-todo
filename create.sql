96a6b1b078eecd99bd96f3229f0cd03ad51c7fb2
lucas.lsilvalr@gmail.com <lucas.lsilvalr@gmail.com>
2022-03-30 22:06:44 -0300

    adição de uma nova tabela e insert de mais linhas

--- create.sql
+++ create.sql
@@ -1,6 +1,14 @@
 CREATE DATABASE TASKS
 
+
+CREATE TABLE [USERS](
+[ID] int identity (1, 1) primary key not null,
+[USER] [VARCHAR] (50) not null);
+
 CREATE TABLE [TAREFAS]
-([ID] [INT] not null,
+([ID] int identity (1, 1) primary key not null,
 [DESCRIPTION] [VARCHAR] (80) not null,
-[CREATIONDATE] [DATE] not null);
+[CREATIONDATE] [DATE] not null,
+[RESP] int FOREIGN KEY REFERENCES users(id)
+);
+
--- insert.sql
+++ insert.sql
@@ -1,26 +1,59 @@
 INSERT INTO [dbo].[TAREFAS]
-([ID],
-[DESCRIPTION],
-[CREATIONDATE])
+([DESCRIPTION],
+[CREATIONDATE],
+resp)
 VALUES
-(41,
-'LIMPAR A SALA',
-'2022-03-30');
+('LIMPAR A SALA',
+'2022-03-30',1
+);
 
 INSERT INTO [dbo].[TAREFAS]
-(ID,
-[DESCRIPTION],
-CREATIONDATE)
+([DESCRIPTION],
+[CREATIONDATE],
+resp)
 VALUES
-(42,
+(
 'BANHO NA ZOE',
-'2022-03-30');
+'2022-03-30',
+1
+);
 
 INSERT INTO [dbo].[TAREFAS]
-(ID,
-[DESCRIPTION],
-CREATIONDATE)
+([DESCRIPTION],
+[CREATIONDATE],
+resp)
 VALUES
-(43,
-'FAZER COMPRAS',
-'2022-03-30');
+('FAZER COMPRAS',
+'2022-03-30',2
+);
+ 
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'limpar a sala' ,	'2022-03-30' ,	'1' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'banho zoe' ,	'2022-03-30' ,	'1' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'fazer compras' ,	'2022-03-30' ,	'2' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'lavar louça' ,	'2022-03-31' ,	'3' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'levar lixo' ,	'2022-03-29' ,	'4' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'lavar banheiro' ,	'2022-03-29' ,	'3' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'fazer janta' ,	'2022-03-27' ,	'2' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'lavar roupa' ,	'2022-03-28' ,	'4' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'arrumar cama' ,	'2022-03-10' ,	'1' 	)
+INSERT INTO TAREFAS(description, CREATIONDATE, resp) VALUES (	'passeio zoe' ,	'2022-03-25' ,	'4' 	)
+
+ INSERT INTO [dbo].[USERS](
+ [USER])
+ VALUES(
+ 'dudu');
+  
+  INSERT INTO [dbo].[USERS](
+ [user])
+ VALUES(
+ 'lira');
+  
+  INSERT INTO [dbo].[USERS](
+ [USER])
+ VALUES(
+ 'babi');
+  
+  INSERT INTO [dbo].[USERS](
+ [user])
+ VALUES(
+ 'vi');
